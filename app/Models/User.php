d92e327d351aea525f0fefb67139a5c2a3dcb352
sklaravel <payel333+spade@gmail.com>
2023-01-19 11:21:03 +0530

    modified user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,13 +4,40 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+//class User extends Model
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+    
+    protected $table = "users";
+    // Need to work on this, I normally avoid autonumber columns to setup primary key
+    protected $primary_key = "id";
+
+    
+    # Quick sample of accessor + mutator
+    // Please note to do the following we can use controller, this is just an example
+    public function setPasswordAttribute($value) {
+        return $this->attributes['password'] = md5($value);
+    }
+
+    public function setNameAttribute($value) {
+        return $this->attributes['name'] = ucwords($value); // return keyword is optional
+    }
+
+
+    public function getGenderAttribute($value) {
+        $userGender = $this->attributes['gender'];
+        // will work on this later.
+
+
+    }
+
 
     /**
      * The attributes that are mass assignable.
