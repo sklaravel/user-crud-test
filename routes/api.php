5ca6a7e4f10da89a21b423547a6532397f638949
sklaravel <payel333+spade@gmail.com>
2023-01-19 11:12:50 +0530

    added web and API routs to project

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/view_all', [UserController::class, 'jsonapi'])->name('view_all');
+
+Route::get('/view_one/{id}', [UserController::class, 'show'])->name('view_one');
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,20 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::get('/', [UserController::class, 'index'])->name('view_user');
+
+Route::get('/view_user', [UserController::class, 'index'])->name('view_user');
+
+Route::get('/create_user', [UserController::class, 'create'])->name('create_user');;
+
+Route::post('/store_user', [UserController::class, 'store'])->name('store_user');
+
+Route::get('/edit_user/{Users}', [UserController::class, 'edit'])->name('edit_user');
+
+Route::post('/update_user/{id}', [UserController::class, 'update'])->name('update_user');
+
+Route::delete('/delete_user/{id}', [UserController::class, 'destroy'])->name('delete_user');
